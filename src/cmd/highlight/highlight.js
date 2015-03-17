8b34a27f5226db417fa3008a8e54c4a300a62848
Boris Schäling <Boris.Schaeling@iomash.com>
2015-03-17 14:05:47 +0100

    Add highlight command

---
+++ src/cmd/highlight/highlight.js
@@ -0,0 +1,14 @@
+define(['jquery', '//cdn.jsdelivr.net/highlight.js/8.4/highlight.min.js'], function($, hljs) {
+    'use strict';
+    return function(args, ctx) {
+        var i = $.inArray('--style', args), style = 'default';
+        if (i !== -1 && i + 1 < args.length) {
+            style = args[i + 1];
+        }
+        $(document.head).append('<link rel="stylesheet" href="//cdn.jsdelivr.net/highlight.js/8.4/styles/' + style + '.min.css">');
+        $(args[1]).each(function(i, block) {
+            hljs.highlightBlock(block);
+        });
+        return ctx.done;
+    };
+});
