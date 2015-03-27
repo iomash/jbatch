0b201d71fe460150779b9bc9239d95a1d41cb4ea
Boris Schäling <Boris.Schaeling@iomash.com>
2015-03-28 00:15:47 +0100

    Fix disqus command

--- src/cmd/disqus/disqus.js
+++ src/cmd/disqus/disqus.js
@@ -1,7 +1,15 @@
-define(function() {
+define(['jquery'], function($) {
     'use strict';
     return function(args, ctx) {
-        var div, disqus = document.getElementById('disqus-js');
+        var i = $.inArray('--appendTo', args), disqus;
+        if (i !== -1 && i + 1 < args.length) {
+            $(args[i + 1]).append('<div id="disqus_thread"></div>');
+        }
+        i = $.inArray('--prependTo', args);
+        if (i !== -1 && i + 1 < args.length) {
+            $(args[i + 1]).prepend('<div id="disqus_thread"></div>');
+        }
+        disqus = document.getElementById('disqus-js');
         if (!disqus) {
             disqus = document.createElement('script');
             disqus.id = 'disqus-js';
@@ -9,9 +17,6 @@ define(function() {
             disqus.src = '//' + args[args.length - 1] + '.disqus.com/embed.js';
             (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(disqus);
         }
-        div = document.createElement('div');
-        div.id = 'disqus_thread';
-        ctx.write(div);
         return ctx.done;
     };
 });
