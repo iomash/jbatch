018be5338b3e2ad223379a0989199501967ecbbe
Boris Schäling <Boris.Schaeling@iomash.com>
2015-03-30 00:16:46 +0200

    Fix twitter command

--- src/cmd/twitter/twitter.js
+++ src/cmd/twitter/twitter.js
@@ -1,35 +1,40 @@
 define(['jquery'], function($) {
     'use strict';
     return function(args, ctx) {
-        var a, i, size = '', showCount = false, showScreenName = true, twitter = document.getElementById('twitter-wjs');
+        var a, i, twitter;
+        if (args[1] === 'follow') {
+            a = document.createElement('a');
+            a.href = 'https://twitter.com/' + args[args.length - 1];
+            a.className = 'twitter-follow-button';
+            i = $.inArray('--size', args);
+            if (i !== -1 && i + 1 < args.length) {
+                a.dataset.size = args[i + 1];
+            }
+            i = $.inArray('--showCount', args);
+            if (i !== -1 && i + 1 < args.length) {
+                a.dataset.showCount = args[i + 1];
+            }
+            i = $.inArray('--showScreenName', args);
+            if (i !== -1 && i + 1 < args.length) {
+                a.dataset.showScreenName = args[i + 1];
+            }
+        } else {
+            return ctx.fail;
+        }
+        i = $.inArray('--appendTo', args);
+        if (i !== -1 && i + 1 < args.length) {
+            $(args[i + 1]).append(a);
+        }
+        i = $.inArray('--prependTo', args);
+        if (i !== -1 && i + 1 < args.length) {
+            $(args[i + 1]).prepend(a);
+        }
+        twitter = document.getElementById('twitter-wjs');
         if (!twitter) {
             twitter = document.createElement('script');
             twitter.id = 'twitter-wjs';
             twitter.src = '//platform.twitter.com/widgets.js';
             (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(twitter);
-        }
-        if (args[1] === 'follow') {
-            i = $.inArray('--size', args);
-            if (i !== -1 && i + 1 < args.length) {
-                size = args[i + 1];
-            }
-            i = $.inArray('--showCount', args);
-            if (i !== -1 && i + 1 < args.length) {
-                showCount = args[i + 1];
-            }
-            i = $.inArray('--showScreenName', args);
-            if (i !== -1 && i + 1 < args.length) {
-                showScreenName = args[i + 1];
-            }
-            a = document.createElement('a');
-            a.href = 'https://twitter.com/' + args[args.length - 1];
-            a.className = 'twitter-follow-button';
-            a.dataset.size = size;
-            a.dataset.showCount = showCount;
-            a.dataset.showScreenName = showScreenName;
-            ctx.write(a);
-        } else {
-            return ctx.fail;
         }
         return ctx.done;
     };
