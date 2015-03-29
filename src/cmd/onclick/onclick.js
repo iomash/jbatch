028350457a0a1b88f4edb251c93fbd0a06488a32
Boris Schäling <Boris.Schaeling@iomash.com>
2015-03-29 23:54:16 +0200

    Prevent default action for click events on links with no href targets

--- src/cmd/onclick/onclick.js
+++ src/cmd/onclick/onclick.js
@@ -1,8 +1,11 @@
 define(['jquery'], function($) {
     'use strict';
     return function(args, ctx) {
-        $(args[1]).click(function() {
+        $(args[1]).click(function(ev) {
             args[2](function(){}, function(){});
+            if (ev.target.tagName === 'A' && !ev.target.getAttribute('href')) {
+                return false;
+            }
         });
         return ctx.done;
     };
